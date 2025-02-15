f0e373a21d82b70c565cd78265363c942117e12d
PaulRB04 <paul.rb2004@gmail.com>
2025-02-15 21:33:38 +0100

    Pick-ups y Game manager

---
+++ AWarrior'sPeace_Project/Assets/AWP_Root/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.Log("Game Manager is null");
+            }
+
+            return _instance;
+        }
+    }
+
+    //Variables (Siempre públicas)
+    public int points;
+    public int winPoints;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void PointsUp(int gain)
+    {
+        points += gain;
+    }
+}
---
+++ AWarrior'sPeace_Project/Assets/AWP_Root/Scripts/PickUps.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUps : MonoBehaviour
+{
+    //Para examen: poner la líne 
+    //public int pointSum;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.PointsUp(1);
+            gameObject.SetActive(false);
+        }
+    }
+}
