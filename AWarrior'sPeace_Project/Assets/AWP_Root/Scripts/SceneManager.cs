e6ba8d63065e7467a41947af4ef1715e1db49767
Izquierdo768 <jizquierdoolmos@gmail.com>
2025-02-17 01:48:29 +0100

    Añadido a
    SceneManager ya esta completado creo pero no estoy segur

---
+++ AWarrior'sPeace_Project/Assets/AWP_Root/Scripts/SceneManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public int specificSceneToLoad;
+
+
+    public void SceneLoader(int sceneToLoad)
+    {
+        //Esta acción carga la escena del espacio igual al entero que indiquemos
+        SceneManager.LoadScene(sceneToLoad);
+    
+    
+    }
+
+    public void SceneLoaderOnPlay()
+    {
+
+        SceneManager.LoadScene(specificSceneToLoad);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    
+    }
+}
